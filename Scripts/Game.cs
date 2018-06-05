af400b61b72dfa8788c1d5f0dedd766e25f8123a
Chrzęścibosz Centralny <mrocznaklawiatura@gmail.com>
2018-06-05 21:01:35 +0200

    MapInfo class added

---
+++ Scripts/Game.cs
@@ -0,0 +1,17 @@
+using Godot;
+using System;
+
+public class Game : Container
+{
+    public override void _Ready()
+    {
+        MapInfo map=new MapInfo("Maps");
+        GD.Print(map.AudioPath);
+        GD.Print(map.ThemePath);
+        GD.Print(map.ChunkAmount);
+        GD.Print(map.ChunkHeight);
+        for (int i = 0; i < map.Blocks.Count; i++)
+            for (int j = 0; j < map.Blocks[i].Count; j++)
+                GD.Print(map.Blocks[i][j]);
+    }
+}
---
+++ Scripts/MapInfo.cs
@@ -0,0 +1,166 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+using System.IO;
+class MapInfo
+{
+    private int chunkAmount;
+    private int chunkHeight;
+    private string audioPath;
+    private string themePath;
+    private List<string> fileContent;
+    private List<List <string>> blocks=new List<List<string>>();
+    private List<int> layers=new List<int>();
+    private string pathToMapFolder;
+
+    public int ChunkAmount
+    {
+        set
+        {
+            chunkAmount=value;
+        }
+        get
+        {
+            return chunkAmount;
+        }
+    }
+    public int ChunkHeight
+    {
+        set
+        {
+            chunkHeight=value;
+        }
+        get
+        {
+            return chunkHeight;
+        }
+    }
+    public string AudioPath
+    {
+        set
+        {
+            audioPath=value;
+        }
+        get
+        {
+            return audioPath;
+        }
+    }
+    public string ThemePath
+    {
+        set
+        {
+            themePath=value;
+        }
+        get
+        {
+            return themePath;
+        }
+    }
+    public List<List<string>> Blocks
+    {
+        get
+        {
+            return blocks;
+        }
+    }
+
+    public MapInfo(){}
+    public MapInfo(string pathToMapFolder)
+    {
+        this.pathToMapFolder=pathToMapFolder;
+        Load();
+    }
+    private void Load()
+    {
+        fileContent=loadFileContent("/map.map");
+        loadChunkAmount();
+        loadChunkHeight();
+        loadAudioPath();
+        loadThemePath();
+        for (int i = 0; i < chunkAmount; i++)
+            loadBlocks(i+1);
+    }
+    private List<string> loadFileContent(string pathSuffix)
+    {
+        string pathToMapHeaderFile = pathToMapFolder + pathSuffix;
+        GD.Print(pathToMapHeaderFile);
+        var lines=System.IO.File.ReadLines(pathToMapHeaderFile);
+        List<string> result = new List<string>(lines);
+        return result;
+    }
+    private void loadChunkAmount()
+    {
+        int headerLine = findHeaderType("chunkCount");
+        GD.Print(headerLine);
+        chunkAmount=Int32.Parse(loadHeaderData(headerLine));
+    }
+    private void loadChunkHeight()
+    {
+        int headerLine = findHeaderType("chunkHeight");
+        chunkHeight=Int32.Parse(loadHeaderData(headerLine));
+    }
+    private void loadAudioPath()
+    {
+        int headerLine = findHeaderType("audio");
+        audioPath=loadHeaderData(headerLine);
+    }
+    private void loadThemePath()
+    {
+        int headerLine = findHeaderType("theme");
+        themePath=loadHeaderData(headerLine);
+    }
+    private int findHeaderType(string type)
+    {
+        for (int i = 0; i < fileContent.Count; i++)
+            for (int j = 0; j < type.Length; j++)
+            {
+                if (type[j] != fileContent[i][j])
+                    break;
+                if(j==type.Length-1)
+                    return i;
+            }
+        return -1; //error
+    }
+    private string loadHeaderData(int lineNr)
+    {
+        if (lineNr >= fileContent.Count || lineNr < 0)
+            return "ERROR: 001"; //param out of range
+        string data="";
+        bool colonEncountered = false;
+        for (int i = 0; i < fileContent[lineNr].Length; i++)
+        {
+            if (colonEncountered)
+                data += fileContent[lineNr][i];
+            if ( fileContent[lineNr][i] == ':')
+                colonEncountered = true;
+        }
+        return data;
+    }
+    private void loadBlocks(int chunkNr)
+    {
+        List<string> chunkRaw=loadFileContent("/chunk"+chunkNr+".chn");
+        layers.Add(chunkRaw.Count/chunkHeight);
+        List<string> chunkFormatted=loadSingleBlocks(chunkRaw);
+        blocks.Add(chunkFormatted);
+    }
+    private List<string> loadSingleBlocks(List<string> source)
+    {
+        List<string> result=new List<string>();
+        for (int i = 0; i < source.Count; i++)
+        {
+            string dataGetter="";
+            for (int j = 0; j < source[i].Length; j++)
+            {
+                if(source[i][j]!=' ')
+                    dataGetter += source[i][j];
+                if(j%4==3)
+                {
+                    result.Add(dataGetter);
+                    dataGetter="";
+                }
+            }
+        }
+        return result;
+    }
+}
