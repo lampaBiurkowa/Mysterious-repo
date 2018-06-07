43f110418d28c1857a44be534c26ddbc4863ce9b
Chrzęścibosz Centralny <mrocznaklawiatura@gmail.com>
2018-06-07 17:02:44 +0200

    Chunk and Layer added

---
+++ Scripts/Chunk.cs
@@ -0,0 +1,5 @@
+using System.Collections.Generic;
+struct Chunk
+{
+    public List<Layer> layers;
+}
---
+++ Scripts/Layer.cs
@@ -0,0 +1,7 @@
+using System.Collections.Generic;
+struct Layer
+{
+    public int xBlockCount;
+    public int yBlockCount;
+    public List<Block> blocks; 
+}
